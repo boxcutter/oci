7fc491e57865f9b2e7fb5bd949ab7eddf43a4b70
Mischa Taylor <taylor@linux.com>
2022-09-25 20:17:52 -0400

    build-local and test should use default tag not latest

--- bin/build-local.sh
+++ bin/build-local.sh
@@ -4,11 +4,11 @@ set -eu
 set -o pipefail
 
 BIN_DIR="$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}")")"
-FULL_IMAGE_NAME="$("${BIN_DIR}"/full-image-name.sh)"
+DEFAULT_TAG="$("${BIN_DIR}/list-tags.sh" | head -n 1)"
 
-echo "==> Building ${FULL_IMAGE_NAME}"
+echo "==> Building ${DEFAULT_TAG}"
 docker buildx build \
   --load \
   --file Containerfile \
-  --tag "${FULL_IMAGE_NAME}" \
+  --tag "${DEFAULT_TAG}" \
   .
