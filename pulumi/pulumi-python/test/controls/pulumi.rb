53730dd040ea206d748d5fdee428477deb8215d4
Mischa Taylor <taylor@linux.com>
2023-02-05 23:54:09 -0500

    Modify check for python in tests

--- pulumi/pulumi-python/test/controls/pulumi.rb
+++ pulumi/pulumi-python/test/controls/pulumi.rb
@@ -13,5 +13,5 @@ end
 
 describe command('python --version') do
   its('exit_status') { should eq 0 }
-  its('stdout') { should match(/Python 3.8/) }
+  its('stdout') { should match(/Python 3.10/) }
 end
