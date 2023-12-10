f96b2e87356a5097584fca4674f97e8fc60df8bb
Mischa Taylor <taylor@linux.com>
2023-12-10 14:22:03 -0500

    Check docker-entrypoint.sh

--- postgres/15/jammy/test/controls/postgres.rb
+++ postgres/15/jammy/test/controls/postgres.rb
@@ -6,3 +6,8 @@ describe command('postgres --version') do
   its('exit_status') { should eq 0 }
   its('stdout') { should match(/postgres \(PostgreSQL\)/) }
 end
+
+describe file('/usr/local/bin/docker-entrypoint.sh') do
+    it { should exist }
+    its('mode') { should cmp '0755' }
+end
