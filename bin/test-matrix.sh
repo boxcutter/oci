b1cbf67ca27d443207b64acd4d0becad73ebbe52
Mischa Taylor <taylor@linux.com>
2024-01-02 16:57:27 -0500

    Pass through any parameters on to single image test script

--- bin/test-matrix.sh
+++ bin/test-matrix.sh
@@ -4,34 +4,44 @@ set -eu -o pipefail
 
 BIN_DIR="$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-tags="$(docker buildx bake local --print 2>/dev/null | jq -r '.target[] | .tags[0]')"
+matrix_test() {
+  tags="$(docker buildx bake local --print 2>/dev/null | jq -r '.target[] | .tags[0]')"
 
-# Initialize an array to store the exit codes
-exit_codes=()
+  # Initialize an array to store the exit codes
+  exit_codes=()
 
-while read -r tag; do
-  echo "==> Testing ${tag}"
-  "${BIN_DIR}/test.sh" "${tag}" &
-  pids+=($!)
-done <<< "${tags}"
+  while read -r tag; do
+    echo "==> Testing ${tag}"
+    "${BIN_DIR}/test.sh" "${tag}" &
+    pids+=($!)
+  done <<< "${tags}"
 
-# Wait for all processes to finish and capture their exit codes
-for pid in "${pids[@]}"; do
-  wait $pid
-  exit_codes+=($?)
-done
+  # Wait for all processes to finish and capture their exit codes
+  for pid in "${pids[@]}"; do
+    wait $pid
+    exit_codes+=($?)
+  done
 
-# Print the collated exit codes
-echo "Exit Codes: ${exit_codes[@]}"
+  # Print the collated exit codes
+  echo "Exit Codes: ${exit_codes[@]}"
 
-# Calculate the overall exit code (e.g., 0 if all succeeded, non-zero if any failed)
-overall_exit_code=0
-for code in "${exit_codes[@]}"; do
-  if [ "$code" -ne 0 ]; then
-    overall_exit_code=$code
-    break
-  fi
-done
+  # Calculate the overall exit code (e.g., 0 if all succeeded, non-zero if any failed)
+  overall_exit_code=0
+  for code in "${exit_codes[@]}"; do
+    if [ "$code" -ne 0 ]; then
+      overall_exit_code=$code
+      break
+    fi
+  done
 
-# Exit with the overall exit code
-exit $overall_exit_code
+  # Exit with the overall exit code
+  exit $overall_exit_code
+}
+
+if docker buildx bake --print 2>/dev/null | jq -e '.group' >/dev/null 2>&1; then
+  echo "==> Matrix build detected"
+  matrix_test
+else
+  echo "==> Non-matrix build detected"
+  "${BIN_DIR}/test.sh" "$@"
+fi
