df629b85f22952c890147700b580abd68f38d73c
Mischa Taylor <taylor@linux.com>
2024-01-02 14:29:54 -0500

    Would help if I actually checked in the scripts

---
+++ bin/image-description.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+json_data="$(docker buildx bake --print 2>/dev/null)"
+first_build_target=$(echo "$json_data" | jq -r '.target | to_entries[0].value')
+echo "$first_build_target" | jq -r '.labels."org.opencontainers.image.description"'
---
+++ bin/image-readme.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+json_data="$(docker buildx bake --print 2>/dev/null)"
+first_build_target=$(echo "$json_data" | jq -r '.target | to_entries[0].value')
+if echo "$first_build_target" | jq -e '.labels."dev.boxcutter.image.readme-filepath"' > /dev/null; then
+  readme_filepath=$(echo "$first_build_target" | jq -r '.labels."dev.boxcutter.image.readme-filepath"')
+  echo "$readme_filepath"
+else
+  echo "${PWD##*}/README.md"
+fi
