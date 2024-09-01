675c54c4509193517bf63600ea6979a381611372
Mischa Taylor <taylor@linux.com>
2024-09-01 12:30:31 -0400

    Remove namespace in name if it exists

--- bin/image-name.sh
+++ bin/image-name.sh
@@ -4,4 +4,7 @@ set -eu -o pipefail
 
 json_data="$(docker buildx bake --print 2>/dev/null)"
 first_build_target=$(echo "$json_data" | jq -r '.target | to_entries[0].value')
-echo "$first_build_target" | jq -r '.labels."org.opencontainers.image.title"'
+image_title=$(echo "$first_build_target" | jq -r '.labels."org.opencontainers.image.title"')
+# if there is a namespace, remove it
+image_name="${image_title##*/}"
+echo "$image_name"
