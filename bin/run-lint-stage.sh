24c2bda527803d6e2b30088103f9587c3a055b8b
Mischa Taylor <taylor@linux.com>
2024-11-19 08:52:50 -0500

    Disable lint stage for hadolint

---
+++ bin/run-lint-stage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+json_data="$(docker buildx bake --print 2>/dev/null)"
+first_build_target=$(echo "$json_data" | jq -r '.target | to_entries[0].value')
+if echo "$first_build_target" | jq -e '.labels."dev.boxcutter.test.container-build-publish-action.run-lint-stage"' > /dev/null; then
+  run_lint_stage=$(echo "$first_build_target" | jq -r '.labels."dev.boxcutter.test.container-build-publish-action.run-lint-stage"')
+  echo "$run_lint_stage"
+else
+  echo "true"
+fi
