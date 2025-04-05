5e4b001d25f63c136c7f72c83814b44dd9b42095
Mischa Taylor <taylor@linux.com>
2025-04-05 12:40:21 -0400

    Fix tests for glab

--- glab/test/controls/glab.rb
+++ glab/test/controls/glab.rb
@@ -1,6 +1,6 @@
 describe command('glab --version') do
   its('exit_status') { should eq 0 }
-  its('stdout') { should match(/glab version/) }
+  its('stdout') { should match(/glab/) }
 end
 
 describe command('docker') do
