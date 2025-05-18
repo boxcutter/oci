a4c8439721ca70b93114cb5a5a81a5a47bf29813
Mischa Taylor <taylor@linux.com>
2025-05-18 15:25:14 -0400

    Bump node 20 to 20.19.2

--- node/20/noble-slim/test/controls/node.rb
+++ node/20/noble-slim/test/controls/node.rb
@@ -8,6 +8,12 @@ describe file('/usr/local/bin/docker-entrypoint.sh') do
   its('mode') { should cmp '0755' }
 end
 
+describe user('node') do
+  it { should exist }
+  its('uid') { should eq 999 }
+  its('gid') { should eq 999 }
+end
+
 describe user('ubuntu') do
   it { should exist }
   its('uid') { should eq 1000 }
