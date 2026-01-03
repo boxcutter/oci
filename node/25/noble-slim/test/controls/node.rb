fd385330c435f45b6065290bd2c4050748524f3f
Mischa Taylor <taylor@linux.com>
2026-01-03 17:45:15 -0500

    Add node 25

---
+++ node/25/noble-slim/test/controls/node.rb
@@ -0,0 +1,19 @@
+describe command('node --version') do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should match(/v25/) }
+end
+
+describe file('/usr/local/bin/docker-entrypoint.sh') do
+    it { should exist }
+    its('mode') { should cmp '0755' }
+end
+
+describe user('node') do
+  it { should exist }
+  its('uid') { should eq 1000 }
+  its('gid') { should eq 1000 }
+end
+
+describe user('ubuntu') do
+  it { should_not exist }
+end
